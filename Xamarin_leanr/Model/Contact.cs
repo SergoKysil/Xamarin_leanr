8e921bf235a40a42570a3ea2a1ec2be5d419bc07
Sergo <kisluy19@ukr.net>
2020-03-20 15:53:15 +0200

    Add Contact model

---
+++ Xamarin_leanr/Model/Contact.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+
+
+namespace Xamarin_leanr.Model
+{
+    [Table ("Contacts")]
+    public class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string ContactName { get; set; }
+        public string ContactSurname { get; set; }
+        public string Gender { get; set; }
+        public string DateOfBirthday { get; set; }
+        public string Address { get; set; }
+        public string  MobileNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
