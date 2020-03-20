92ab10558b921ed128cc840c5a237209d6c0ab95
Sergo <kisluy19@ukr.net>
2020-03-20 18:03:12 +0200

    Add services

---
+++ Xamarin_leanr/Services/ContactRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin_leanr.Data;
+using Xamarin_leanr.Model;
+
+namespace Xamarin_leanr.Services
+{
+    class ContactRepository : IContactRepository
+    {
+
+        DataFunctions _dataFunctions;
+        public ContactRepository()
+        {
+            _dataFunctions = new DataFunctions();
+        }
+
+        public void AddNewContact(Contact contact)
+        {
+            _dataFunctions.AddNewContact(contact);
+        }
+
+        public void DeleteAllContacts()
+        {
+            _dataFunctions.DeleteAllContacts();
+        }
+
+        public void DeleteContact(int contactId)
+        {
+            _dataFunctions.DeleteContact(contactId);
+        }
+
+        public List<Contact> GetAllContacts()
+        {
+            return _dataFunctions.GetAllContacts();
+        }
+
+        public Contact GetContact(int contactId)
+        {
+            return _dataFunctions.GetContact(contactId);
+        }
+
+        public void UpdateContact(Contact contact)
+        {
+            _dataFunctions.UpdateContact(contact);
+        }
+    }
+}
---
+++ Xamarin_leanr/Services/IContactRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin_leanr.Model;
+
+namespace Xamarin_leanr.Services
+{
+    public interface IContactRepository
+    {
+
+        List<Contact> GetAllContacts();
+
+        Contact GetContact(int contactId);
+
+        void DeleteAllContacts();
+
+        void DeleteContact(int contactId);
+
+        void AddNewContact(Contact contact);
+
+        void UpdateContact(Contact contact);
+    }
+}
