0283e69290b5e974f3a930c60fead67521321618
Sergo <kisluy19@ukr.net>
2020-03-23 21:25:43 +0200

    Add ValidatorToDo

---
+++ Xamarin_leanr/Validator/ToDoValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+using Xamarin_leanr.Model;
+
+namespace Xamarin_leanr.Validator
+{
+    class ToDoValidator : AbstractValidator<ToDo>
+    {
+        public ToDoValidator()
+        {
+            RuleFor(t => t.Title).Must(n => ValidateString(n)).WithMessage("Головна назва повинна бути заповнена!");
+            RuleFor(t => t.Content).Must(n => ValidateString(n)).WithMessage("Додайте опис!");
+        }
+
+        public bool ValidateString(string stringValue)
+        {
+            if (!string.IsNullOrEmpty(stringValue))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
